62256db82e6d881e723ea95d2d57a7f97d3e911c
oyama136051 <oyama.jeed@kinki.jpn.org>
2023-05-16 12:02:53 +0100

    [mod05161102]k06s.c

--- k06s.c
+++ k06s.c
@@ -1,75 +1,116 @@
-/*** TCP/IP server program ***/
-/* Usage: ./server HostName */
+//*** kadai06s.c ***/
+/* TCP/IP echo server */
+/*     Usage: ./k06s <server port> */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
 
-#define PORT 50001
+#define BUF_LEN 32
+#define QUEUE_MAX 5
 
-char buf[]="Sample message.\n";
+/* PROTOTYPE */
+void clHandler(int clSock);  // Client Handler
+void excep(char *errMsg);    // Exception
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_in ServerAddr;
-    struct sockaddr_in ClientAddr;
-    struct hostent *HostInfo;
-    int ServerSessionID;
-    int ClientSessionID;
-    int clientlength;
-  
-    if (argc!=2){
-        fprintf(stderr,"Usage: ./server HostName!\n");
-        exit(1);
-    }
-  
-    if ((ServerSessionID=socket(AF_INET,SOCK_STREAM,0))==-1){
-        fprintf(stderr, "Failed to make a socket!\n");
-        exit(1);
-    }
-  
-    if ((HostInfo=gethostbyname(argv[1]))==NULL){
-        fprintf(stderr,"Failed to find host.\n");
-        exit(1);
-    }
-  
-    ServerAddr.sin_family=AF_INET;
-    ServerAddr.sin_port=htons(PORT);
-    memcpy((char *)&ServerAddr.sin_addr,(char *)HostInfo->h_addr_list[0],
-                                                       HostInfo->h_length);
-    if (bind(ServerSessionID,(struct sockaddr *)&ServerAddr,
-                                                  sizeof(ServerAddr))==-1){
-        fprintf(stderr,"Failed to assign a socketname.\n");
-        exit(1);
-    }
-  
-    if (listen(ServerSessionID,5)==-1){
-        fprintf(stderr,"Failed to listen.\n");
-        exit(1);
-    }
-  
-    while(1){
-        clientlength=sizeof(ClientAddr);
-        if ((ClientSessionID=accept(ServerSessionID,
-          (struct sockaddr *)&ClientAddr,(socklen_t *)&clientlength))==-1){
-            fprintf(stderr,"Failed to accept.\n");
-            exit(1);
-        }
-    
-        if (send(ClientSessionID,buf,sizeof(buf),0)==-1){
-            fprintf(stderr,"Failed to send a message.\n");
-            exit(1);
-        }
-    
-        if (shutdown(ClientSessionID,2)==-1){
-            fprintf(stderr,"Failed to shutdown.\n");
-            exit(1);
-        }
-        close(ClientSessionID);
-    }
+	int svSock;
+	int clSock;
+	struct sockaddr_in svAddr;
+	struct sockaddr_in clAddr;
+	int svPort;
+	int clAddrLen;  // Size of "Client Address Struct"
+
+	/* CHECK ARGS. */
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <server port>\n", argv[0]);
+		exit(1);
+	}
+	svPort = atoi(argv[1]);
+
+	/* CREATE SOCKET */
+	if ((svSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+		excep("FAIL:socket()");
+	}
+
+	/* INITIALIZE ADDRESS STRUCT */
+	memset(&svAddr, 0, sizeof(svAddr));
+	svAddr.sin_family = AF_INET;
+	svAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	svAddr.sin_port = htons(svPort);
+
+	/* BIND (LOCAL SERVER ADDRESS) */
+	if (bind(svSock, (struct sockaddr *)&svAddr, sizeof(svAddr)) == -1) {
+		excep("FAIL:bind()");
+	}
+
+	/* LISTENING */
+	if (listen(svSock, QUEUE_MAX) == -1) {
+		excep("FAIL:listen()");
+	}
+
+	printf("\nLISTENING...");
+
+	while (1) {
+		/* WAITING REQUEST FROM CLIENTS */
+		printf("\naccept()");
+		if ((clSock = accept(svSock, (struct sockaddr *)&clAddr, &clAddrLen)) == -1) {
+    			excep("FAIL:accept()");
+		}
+
+		printf("\nCONNECTED CLIENT : %s", inet_ntoa(clAddr.sin_addr));
+    		printf(", port%d", ntohs(clAddr.sin_port));
+
+		/* CALL CLIENT HANDLER */
+		clHandler(clSock);
+	}
+	// close(svSock);
+	// return 0;
+}
+
+/*** CLIENT HANDLER ***/
+void clHandler(int clSock)
+// int clSock: client socket
+{
+	char echoBuf[BUF_LEN];
+	int bytesRcvd;
+
+	if ((bytesRcvd = recv(clSock, echoBuf, BUF_LEN - 1, 0)) == -1) {
+		excep("FAIL:recv()[1]");
+	}
+	echoBuf[bytesRcvd] = '\0';
+	printf("\nrecv():%s[%dbyte]", echoBuf, bytesRcvd);
+
+	while (bytesRcvd > 0) {
+		/* ECHO-BACK */
+		if (send(clSock, echoBuf, bytesRcvd, 0) != bytesRcvd) {
+			excep("FAIL:send()");
+		}
+		printf("\necho-back sent.");
+
+		/* RECEIVE REMAIN PART */
+		if ((bytesRcvd = recv(clSock, echoBuf, BUF_LEN - 1, 0)) == -1) {
+			excep("FAIL:recv()[2]");
+		}
+		echoBuf[bytesRcvd] = '\0';
+		printf("\nrecv():%s[%dbyte]", echoBuf, bytesRcvd);
+	}
+
+	/* CLOSE SOCKET */
+	close(clSock);
+	printf("\nCONNECTION CLOSED.");
+	return;
+}
+
+void excep(char *errMsg)
+{
+	perror(errMsg);
+	exit(1);
 }
