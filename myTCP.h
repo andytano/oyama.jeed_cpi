1fa780be7a873f3ea8f346946afb2a581619e3fd
oyama136051 <oyama.jeed@kinki.jpn.org>
2023-05-30 11:43:17 +0100

    [add230530]k10c.c,k10s.c,myTCP.h

---
+++ myTCP.h
@@ -0,0 +1,18 @@
+/*** myTCP.h ***/
+/* HEADER FILES for TCP/IP */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <arpa/inet.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
+#include <pthread.h>
+
