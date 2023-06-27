6e74bac7bbc1bdcc5c361848516a97bc808bd72d
oyama136051 <oyama.jeed@kinki.jpn.org>
2023-06-27 18:34:38 +0100

    [mod230627]k12s.c,clThread_12.c

--- clThread_12.c
+++ clThread_12.c
@@ -6,13 +6,13 @@
 void *threadMain(void *thrArgs)
 {
 	int clSock;	// SOCKET DESCRIPTOR for CLIENT
-
+	
 	pthread_detach(pthread_self());		// --> RELEASE RESOURCES
-
+	
 	/* ARGUMENTS */
 	clSock = ((struct threadArgs *)thrArgs)->clSock;
 	free(thrArgs);
-
+	
 	/* HANDLE A CLIENT REQUEST */
 	clHandler(clSock);
 	return (NULL);
@@ -21,15 +21,48 @@ void *threadMain(void *thrArgs)
 
 /*** HANDLE A CLIENT REQUEST ***/
 void clHandler(int clSock)
-	// int clSock: SOCKET DESCRIPTOR for CLIENT
+	// int clSock: client socket
 {
-	//char buf[BUF_LEN];
-	//int bytesRcvd;
-	//char replyBuf[R_BUF_LEN];
-	//char *bufPtr;
-	//char *nextPtr;
-	//int i;
-	//char str[6];
+	char buf[BUF_LEN];
+	int bytesRcvd;
+	char *bufPtr;
+	char *nextPtr;
+	int i;
+	char str[6];
+	struct httpReqLine line = {NULL, NULL, NULL};
+	struct httpReqHeader head[HEADERS_MAX];
+
+	/* 1行受信 */
+	bytesRcvd = recvOneLine(clSock, buf);
+	pr_msg("recv():", buf);
+	numToStr(bytesRcvd, str, 5);
+	pr_msg("<SIZE> ", str);
+
+	/* トークン取り出し */
+	line.method = strtok_re(buf, " \t\r\n", &nextPtr);
+	pr_msg("METHOD:", line.method);
+	line.uri = strtok_re(NULL, " \t\r\n", &nextPtr);
+	pr_msg("URI:", line.uri);
+	line.httpVer = strtok_re(NULL, " \t\r\n", &nextPtr);
+	pr_msg("HTTP_VER:", line.httpVer);
+	
+	bufPtr = nextPtr;
+	for (i = 0; i < HEADERS_MAX; i++) {
+		/* 1行ずつ受信 */
+		bytesRcvd = recvOneLine(clSock, bufPtr);
+		if (bytesRcvd == 0) break;
+		numToStr(bytesRcvd, str, 5);
+		pr_msg("<SIZE> ", str);
+		pr_msg("recv():", bufPtr);
+
+		/* トークン取り出し */
+		numToStr(i, str, 3);
+		pr_msg("HEAD No.", str);
+		head[i].name = strtok_re(bufPtr, ":", &nextPtr);
+		pr_msg("HEAD_NAME:", head[i].name);
+		head[i].val = strtok_re(NULL, "\t\r\n", &nextPtr);
+		pr_msg("HEAD_VAR:", head[i].val);
+	}
 
 	/* CLOSE SOCKET */
 	if (shutdown(clSock, 2) == -1) {
@@ -39,3 +72,5 @@ void clHandler(int clSock)
 	pr_msg("CONNECTION CLOSED.", "\n");
 	return;
 }
+
+
--- k12s.c
+++ k12s.c
@@ -2,7 +2,45 @@
 /* HTTP SERVER PART 1              */
 /*     Usage: ./k12s <server port> */
 
-int main(/*int argc, char *argv[]*/)
-{
+#include "./myTCP.h"
 
+/*** MAIN ROUTINE ***/
+int main(int argc, char *argv[])
+{
+	int svSock;										// SOCKET DESCRIPTOR for SERVER
+	int clSock;										// SOCKET DESCRIPTOR for CLIENT
+	unsigned short svPort;				// PORT NUMBER for SERVER
+	pthread_t threadID;						// THREAD ID
+	struct threadArgs *thrArgs;		// POINTER OF THREAD ARGUMENT STRUCTURE
+	char thrIDStr[21];
+	
+	/* 初期化 */
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <server port>\n", argv[0]);
+		exit(1);
+	}
+	svPort = atoi(argv[1]);
+	svSock = createTCPSvSock(svPort);
+	
+	/* リクエスト待ちループ */
+	while (1) {
+		/* リクエスト受付け */
+		clSock = acceptTCPConn(svSock);
+		
+		/* スレッド生成 */
+		thrArgs = (struct threadArgs *)malloc(sizeof(struct threadArgs));
+		if (thrArgs == NULL) {
+			excep("FAIL:malloc()");
+		}
+		thrArgs->clSock = clSock;
+		if (pthread_create(&threadID, NULL, threadMain, (void *)thrArgs) != 0) {
+			excep("FAIL:pthread_create()");
+		}
+		numToStr((long int)threadID, thrIDStr, 20);
+		pr_msg("THREAD ID : ", thrIDStr);
+	}
+	// close(svSock);
+	// return 0;
 }
+
+
